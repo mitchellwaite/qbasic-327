ab5fffde23ecd100ecf9c32916dbbed9a136519f
Mitchell Waite <mitch.thepcman@gmail.com>
2017-10-04 18:30:31 -0400

    Initial Commit

---
+++ test/run_test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Runs pytest and dumps stdout to a log file in the "res" directory
+
+pytest | tee "res/$(date "+%Y%h%d_%H%M%S").log"
---
+++ test/test/test_login.py
@@ -0,0 +1,14 @@
+import pytest
+
+@pytest.fixture
+def setup():
+   print "setup fixture"
+
+def test_login(setup):
+   assert(0==1)
+
+def test_login_2(setup):
+   assert(1==1)
+
+def test_login_3(setup):
+   raise ValueError
