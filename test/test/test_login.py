eeeabaee0f2b136c74c05b91bba4019d3f5fffca
Mitchell Waite <mitch.thepcman@gmail.com>
2017-10-15 20:46:01 -0400

    Attempts with pexpect/pytest

--- test/test/test_login.py
+++ test/test/test_login.py
@@ -1,11 +1,26 @@
 import pytest
+import pexpect
+
+mainPyCommand = "python ../../src/main.py"
 
 @pytest.fixture
 def setup():
    print "setup fixture"
 
 def test_login(setup):
-   assert(0==0)
+   child = pexpect.spawn(mainPyCommand)
+   
+   child.sendline("lmao")
+
+   idx = child.expect(['Unknown Command: lmao','(.*?)'])
+   print child.before
+
+   if child.isalive():
+      child.close()
+
+   print "index = {}".format(idx)
+
+   assert(idx == 0)
 
 def test_login_2(setup):
    assert(1==1)
