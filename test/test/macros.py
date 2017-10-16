898c9bcfebce22fdbca79c149c9caad4b2601c50
Mitchell Waite <mitch.thepcman@gmail.com>
2017-10-16 10:44:19 -0400

    Add macros for various testing directories
    Pytest can now be run successfully from anywhere inside the qbasic-327
    directory

--- test/test/macros.py
+++ test/test/macros.py
@@ -1,10 +1,22 @@
 import shutil
 import os
 
+def mainPyLocation():
+   return testHomeDir() + "/../src/main.py"
+
+def testHomeDir():
+   return os.path.dirname(os.path.realpath(__file__)) + "/.."
+
+def testDataDir():
+   return testHomeDir() + '/data'
+
+def testTempDir():
+   return testHomeDir() + '/tmp'
+
 def removeTempDirs():
    try:
-       shutil.rmtree("../tmp")
+       shutil.rmtree(testTempDir())
    except Exception as e:
        pass
 
-   os.makedirs("../tmp")
+   os.makedirs(testTempDir())
--- test/test/test_login.py
+++ test/test/test_login.py
@@ -3,7 +3,8 @@ import pexpect
 import os
 import macros
 
-mainPyCommand = "python ../../src/main.py -o ../tmp -v ../data/frontend/common/valid_accounts.txt"
+mainPyCommand = "python {} -o {} -v {}/frontend/common/valid_accounts.txt".format(macros.mainPyLocation(), macros.testTempDir(), macros.testDataDir())
+mydir = os.path.dirname(os.path.realpath(__file__))
 
 @pytest.fixture
 def setup():
@@ -24,8 +25,8 @@ def run_test(child, input, output):
 def test_login(setup):
     rc = 0
 
-    inputs = [line.strip() for line in open('../data/frontend/login/login2_input.txt')]
-    outputs = [line.strip() for line in open('../data/frontend/login/login2_output.txt')]
+    inputs = [line.strip() for line in open(macros.testDataDir() + '/frontend/login/login2_input.txt')]
+    outputs = [line.strip() for line in open(macros.testDataDir() + '/frontend/login/login2_output.txt')]
 
     #print inputs
     #print outputs
