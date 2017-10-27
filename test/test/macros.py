d5db29d1735e6b2e7a192ca12c0107086ccb21b2
Mitchell Waite <mitch.thepcman@gmail.com>
2017-10-27 17:57:26 -0400

    Update macros.py

--- test/test/macros.py
+++ test/test/macros.py
@@ -43,7 +43,9 @@ def getIoList(inFile, outFile):
 
 def spawnFrontend():
     frontendPyCommand = "python {} -o {} -v {}/frontend/common/valid_accounts.txt".format(mainPyLocation(), testTempDir(), testDataDir())
-    return pexpect.spawn(frontendPyCommand)
+    child = pexpect.spawn(frontendPyCommand);
+    child.expect("> ") #Wait for a prompt
+    return child;
 
 def compare_files(file1, file2):
     result = filecmp.cmp(file1, file2)
@@ -70,21 +72,19 @@ def assertEmptyTestDir():
 
 def run_test(child, input, output):
 
-    if not child.isalive():
-        print "Error: child is dead."
-        return -1
-
-    # Wait for a prompt.
-    idx = child.expect_exact([">",pexpect.TIMEOUT,pexpect.EOF],1)
+    #Make sure the child is still alive.
+    assert child.isalive()
 
     # Send the command
     child.sendline(input)
 
-    # this is a dummy readline, it throws away the sendline from above
-    child.readline()
+    # wait for a prompt again
+    idx = child.expect(["> ",pexpect.TIMEOUT,pexpect.EOF])
 
     # this is the actual output from QBasic that we want
-    actualOutput = child.readline().rstrip()
+    spl = child.before.splitlines()
+    print spl
+    actualOutput = spl[len(spl) - 1] #< right here.
 
     if output != actualOutput:
         print "Error: expected output did not match actual output!\nCommand: {}\nExpected: {}\nActual: {}\n".format(input,output,actualOutput)
