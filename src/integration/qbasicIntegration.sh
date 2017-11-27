6d1379d21794e38f3c97fcf23e3ada31381ab0d5
Mitchell Waite <mitch.thepcman@gmail.com>
2017-11-27 14:59:11 -0500

    Save the merged transaction summary files, too

--- src/integration/qbasicIntegration.sh
+++ src/integration/qbasicIntegration.sh
@@ -14,17 +14,16 @@ for dayFolder in $(ls ./res/userinput);do
    done
 
    # Merge the transaction summary files after each day is run
-   python tsfMerge.py ./out/mergedTsf.txt `find ./out -name *.txt`
+   python tsfMerge.py ./beOut/mergedTsf_$dayFolder.txt `find ./out -name *.txt`
 
    # Run the backend
-   python ../backend/main.py -a ./res/masterAccountsFile.txt -t ./out/mergedTsf.txt -v ./beOut/validAccountsFile_$dayFolder.txt -o ./beOut/masterAccountsFile_$dayFolder.txt
+   python ../backend/main.py -a ./res/masterAccountsFile.txt -t ./beOut/mergedTsf_$dayFolder.txt -v ./beOut/validAccountsFile_$dayFolder.txt -o ./beOut/masterAccountsFile_$dayFolder.txt
 
    # Copy the backend outputs to the res folder so they can be used for
    # the next day
    cp ./beOut/validAccountsFile_$dayFolder.txt ./res/validAccountsFile.txt
    cp ./beOut/masterAccountsFile_$dayFolder.txt ./res/masterAccountsFile.txt
 
-   # Clean up the output directories
-#   rm ./beOut/*
+   # Clean up the output directory
    rm ./out/*
 done
